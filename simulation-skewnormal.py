ac70b77652b10ebb9285c04148633966ba9ddf04
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2021-01-14 16:09:05 +0100

    modified some tests

--- simulation-skewnormal.py
+++ simulation-skewnormal.py
@@ -3,12 +3,17 @@ from jax import jit, grad, random
 from jax.scipy.stats import multivariate_normal as mvn
 from jax.scipy.stats import norm
 
+from jax.config import config
+config.update('jax_enable_x64', True)
+
 from libs.skewnormal import SkewNormal
 from libs.manifold import Product, SPD, Euclidean
 from libs.minimizer import OPTIM
 
-from jax.config import config
-config.update('jax_enable_x64', True)
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('darkgrid')
 
 n = 1000
 p = 2
@@ -33,6 +38,21 @@ rng, key = random.split(rng)
 data = sn.sample(key, shape=(n,))
 data = data - jnp.mean(data, axis=0)
 
+sns.kdeplot(data=pd.DataFrame(data, columns=['x', 'y']),
+            x='x', y='y',
+            fill=True)
+sns.kdeplot(data=pd.DataFrame(
+                random.multivariate_normal(
+                              key, mean=mean, cov=sn.Psi,
+                              shape=(n,)),
+                columns=['x', 'y']),
+            x='x', y='y',
+            levels=[0.15, 0.25, 0.5, 0.75, 0.9],
+            alpha=0.4, color='k', linestyles='--', linewidths=0.8
+            )
+plt.show()
+
+print(sn.Psi, sn.theta)
 
 @jit
 def loglik(X):
@@ -64,6 +84,35 @@ optimizer = OPTIM(
 
 rng, key = random.split(rng)
 init = man.rand(key)
-
+logs = None
 res, logs = optimizer.solve(loglik, gradient, x=init)
 print(res)
+
+if logs is not None:
+    f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(14, 21))
+    # f, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(7, 10))
+
+    ax1.plot(logs.it, logs.fun)
+    # ax1.axhline(y=true_loglik, xmin=0, xmax=res.nit, c='k', ls='--', lw=0.8)
+    ax1.set_yscale('log')
+    ax1.set_ylabel(r'$\mathcal{L}$')
+
+    ax2.plot(logs.it, logs.grnorm)
+    ax2.axhline(y=tol, xmin=0, xmax=res.nit, c='k', ls='--', lw=0.8)
+    ax2.set_yscale('log')
+    ax2.set_ylabel(r'$\log\Vert\nabla\mathcal{L}_\star\Vert$')
+
+    for i in range(p):
+        ax3.plot(logs.it, [logs.x[j][1][i] for j in range(res.nit + 1)])
+        ax3.plot(logs.it, [logs.x[j][0][i, i] for j in range(res.nit + 1)])
+    ax3.plot(logs.it, [logs.x[j][0][0, 1] for j in range(res.nit + 1)])
+
+    # ax3.plot(logs.it, [man.dist((sn.Psi, sn.theta), logs.x[i])
+    #                    for i in range(res.nit + 1)])
+    # ax3.axhline(y=tol, xmin=0, xmax=res.nit, c='k', ls='--', lw=0.8)
+    # ax3.set_yscale('log')
+    # ax3.set_ylabel(r'$\log\left\Vert\hat\Omega - \Omega_\star\right\Vert$')
+
+    f.suptitle('Optimizers performance for {}-variate skew-normal'.format(p))
+    plt.xlabel('Iterations')
+    plt.show()
