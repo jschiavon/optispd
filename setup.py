64cab592c443a3001a268beff9c4e27f91a6cd1a
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2021-01-28 12:22:27 +0100

    added setup file to perform installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='optispd', 
+      version='0.1', 
+      description='Small library to perform optimization on SPD manifold',
+      author='Jacopo Schiavon',
+      url='github.com/jschiavon/optispd',
+      packages=find_packages())
