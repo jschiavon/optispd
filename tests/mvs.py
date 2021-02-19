fb651a44644406b18e9065152dd218c0f113d4db
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2021-02-19 09:25:27 +0100

    skewnormal updates and tests

--- tests/mvs.py
+++ tests/mvs.py
@@ -1,14 +1,17 @@
 import jax.numpy as jnp
-from jax import jit, grad, random
+from jax import jit, grad, jvp, random
 from jax.scipy.stats import multivariate_normal as mvn
 from jax.scipy.stats import norm
+
+from scipy.optimize import minimize, NonlinearConstraint
+
+from jax.config import config
+config.update('jax_enable_x64', True)
 
 from skewnormal import SkewNormal
 from optispd.manifold import Product, SPD, Euclidean
 from optispd.minimizer import minimizer
 
-from jax.config import config
-config.update('jax_enable_x64', True)
 
 n = 1000
 p = 2
@@ -16,18 +19,18 @@ tol = 1e-6
 seed = 42
 rng = random.PRNGKey(seed)
 
-rng, *key = random.split(rng, 5)
+rng, *key = random.split(rng, 6)
+# mean = random.normal(key[0], shape=(p,))
 mean = jnp.zeros(shape=(p,))
-random_gen = random.uniform(key[2], shape=(2 * p,))
-scale, skew = random_gen[:p], random_gen[p:]
+cov = jnp.linalg.qr(random.normal(key[1], shape=(p, p)))[0]
+coveigs = random.uniform(key[2], shape=(p,), minval=2, maxval=20)
+cov = jnp.einsum('ij,j,kj->ik', cov, coveigs, cov)
+theta = random.uniform(key[3], shape=(p,))
+thetanorm = random.uniform(key[4], maxval=min(coveigs))
+theta = theta / (jnp.linalg.norm(theta) / thetanorm)
+skew = jnp.einsum('i,i->i', 1./jnp.sqrt(jnp.diag(cov)), theta)
 
-fir = jnp.prod(skew)
-sec = jnp.sqrt((skew[0]*skew[0] - 1) * (skew[1] * skew[1] - 1))
-cor = random.uniform(key[1], minval=fir-sec, maxval=fir+sec)
-
-cor_mat = jnp.array([[1., cor], [cor, 1.]])
-
-sn = SkewNormal(mean=mean, scale=scale, cor=cor_mat, skew=skew)
+sn = SkewNormal(mean=mean, cov=cov, skew=skew)
 
 rng, key = random.split(rng)
 data = sn.sample(key, shape=(n,))
@@ -35,9 +38,9 @@ data = data - jnp.mean(data, axis=0)
 
 
 @jit
-def loglik(X):
+def loglik(Sigma, theta):
     """Compute the loglikelihood for the skewnormal."""
-    Psi, theta = tuple(X)
+    Psi = Sigma - jnp.outer(theta, theta)
     psitheta = jnp.linalg.solve(Psi, theta)
     alpha = psitheta / jnp.sqrt(1 + jnp.matmul(theta.T, psitheta))
     capital_phi = jnp.sum(norm.logcdf(jnp.matmul(alpha, data.T)))
@@ -45,25 +48,104 @@ def loglik(X):
         mvn.logpdf(
             data,
             mean=jnp.zeros(p),
-            cov=Psi)
+            cov=Psi - jnp.outer(alpha, alpha))
         )
-    expterm = jnp.sum(0.5 * jnp.matmul(alpha, data.T))
-    return - (small_phi + capital_phi + expterm)
+    return - (2 + small_phi + capital_phi)
+    
 
+print("True values:")
+print("\tSigma: {}".format(sn.cov.ravel()))
+print("\t(Eigs: {})".format(jnp.linalg.eigvalsh(sn.cov)))
+print("\tTheta: {} (norm: {})".format(sn.theta, jnp.linalg.norm(sn.theta)))
+print("\tLoglik: {:.2f} (check: {:.2f})".format(loglik(sn.cov, sn.theta), sn.logpdf(data)))
 
-loglik
-gradient = jit(grad(loglik))
-
-man = Product([SPD(p=p), Euclidean(p)])
+man = SPD(p=p)
 
 optimizer = minimizer(
     man, method='rsd',
-    maxiter=200, mingradnorm=tol,
-    verbosity=1, logverbosity=True
+    maxiter=1, mingradnorm=tol,
+    verbosity=0, logverbosity=False
     )
 
-rng, key = random.split(rng)
-init = man.rand(key)
+k = 0
+maxit = 100
 
-res, logs = optimizer.solve(loglik, gradient, x=init)
-print(res)
+rng, *key = random.split(rng, 4)
+sig = man.rand(key[0])
+limtheta = min(jnp.linalg.eigvalsh(sig))
+theta = random.uniform(key[1], shape=(p,))
+th_norm = random.uniform(key[2], maxval=limtheta)
+theta = theta / (jnp.linalg.norm(theta) / th_norm)
+
+logl = [loglik(sig, theta)]
+
+while True:
+    print("Iteration {} starts from:".format(k))
+    print("\tSigma : {}".format(sig.ravel()))
+    print("\t(Eigs: {})".format(jnp.linalg.eigvalsh(sig)))
+    print("\tTheta: {} (norm: {})".format(theta, jnp.linalg.norm(theta)))
+    print("\tLoglik : {:.2f}".format(logl[-1]))
+
+    loglik_sig = jit(lambda x: loglik(x, theta))
+    gradient_sig = jit(grad(loglik_sig))
+
+    res = optimizer.solve(loglik_sig, gradient_sig, x=sig)
+
+    sig = res.x
+
+    print('\t...')
+
+    loglik_th = jit(lambda x: loglik(sig, x))
+    gradient_psi = jit(grad(loglik_th))
+
+    constrain = jnp.min(jnp.linalg.eigvalsh(sig))
+    const_fun = jit(lambda x: jnp.linalg.norm(x) ** 2)
+    jac = jit(grad(const_fun))
+    # hess = jit(lambda x, v: jvp(grad(const_th), (x,), (v,)))
+    
+    if const_fun(theta) > constrain:
+        print('{} > {}, rescaling theta'.format(const_fun(theta), constrain))
+        rng, key = random.split(rng)
+        th_norm = random.uniform(key, maxval=limtheta)
+        theta = theta / (jnp.linalg.norm(theta) / th_norm)
+
+    norm_constr = NonlinearConstraint(const_fun,
+                                      0, constrain,
+                                      jac=jac,
+                                    #   hess=hess
+                                      )
+    # print('{} < {}'.format(const_th(theta), jnp.min(jnp.linalg.eigvalsh(sig))))
+    res = minimize(loglik_th, theta,
+                   method="slsqp",
+                   jac=gradient_psi,
+                   constraints=norm_constr,
+                   tol=tol,
+                   options={'maxiter':5}
+                   )
+    theta = res.x
+
+    logl.append(loglik(sig, theta))
+    k += 1
+
+    print("And ends at:")
+    print("\tSigma : {}".format(sig.ravel()))
+    print("\t(Eigs: {})".format(jnp.linalg.eigvalsh(sig)))
+    print("\tTheta: {} (norm: {})".format(theta, jnp.linalg.norm(theta)))
+    print("\tLoglik : {:.2f}".format(logl[-1]))
+
+    if jnp.isclose(logl[-2], logl[-1], rtol=tol) or k == maxit:
+        break
+
+    if jnp.isnan(logl[-1]).any():
+        print("PANIC! NAN APPEARS")
+        break
+    
+    print("\n---\n")
+
+    
+import matplotlib.pyplot as plt
+
+plt.plot(logl)
+plt.show()
+
+
--- tests/skewnormal.py
+++ tests/skewnormal.py
@@ -1,67 +1,91 @@
 import jax.numpy as jnp
-from jax import jit, grad, random
-from jax.lax import broadcast_shapes
+from jax import jit, random
 from jax.scipy.stats import norm
 from jax.scipy.stats import multivariate_normal as mvn
 
-
-@jit
-def _theta(scale, skewness):
-    return jnp.einsum('i,i->i', scale, skewness)
-
-
-@jit
-def _psi(scale, correlation, skewness):
-    mat = correlation + jnp.outer(skewness, skewness)
-    return jnp.einsum('j,jk,k->jk', scale, mat, scale)
-
-
-@jit
-def _omega(scale, cor, theta):
-    _mat = jnp.einsum('j,jk,k->jk', scale, cor, scale)
-    _fr = jnp.append(_mat, jnp.expand_dims(theta, 1), axis=1)
-    _sr = jnp.expand_dims(jnp.append(theta, 1.), 0)
-    return jnp.append(_fr, _sr, axis=0)
-
-
-@jit
-def _alpha(psi, theta):
-    num = jnp.linalg.solve(psi, theta)
-    den = jnp.sqrt(1 + jnp.matmul(theta.T, num))
-    return num / den
-
-
-class ComputeOnDemand():
-    """Descriptor to lazy compute skewnormal parameters."""
+class theta_on_demand():
+    """Descriptor to lazily compute theta."""
 
     def __init__(self):
-        """Initialize empty cache to lazy compute parameters."""
+        """Initialize empty cache."""
         self.cache = {}
-
-    def __set_name__(self, owner, name):
-        """Generate private attribute and public interface."""
-        self.public_name = name
-        self.private_name = '__' + name
-
+    
     def __get__(self, obj, objtype=None):
-        """Lazy compute: if parameter value is cached, return that."""
         value = self.cache.get(id(obj), None)
         if value is not None:
             return value
-        if self.public_name == 'theta':
-            value = _theta(obj.scale, obj.skew)
-        elif self.public_name == 'Psi':
-            value = _psi(obj.scale, obj.cor, obj.skew)
-        elif self.public_name == 'Omega':
-            value = _omega(obj.scale, obj.cor, obj.theta)
-        elif self.public_name == 'alpha':
-            value = _alpha(obj.Psi, obj.theta)
+        value = jnp.einsum('i,i->i', obj.scale, obj.skew)
         self.cache[id(obj)] = value
         return value
 
-    def __set__(self, obj, value):
-        """Raise if try to set parameter value."""
-        raise ValueError(self.public_name + " is not writable")
+
+# class corr_on_demand():
+#     """Descriptor to lazily compute correlation matrix."""
+
+#     def __init__(self):
+#         """Initialize empty cache."""
+#         self.cache = {}
+    
+#     def __get__(self, obj, objtype=None):
+#         value = self.cache.get(id(obj), None)
+#         if value is not None:
+#             return value
+#         std = 1. / obj.scale
+#         value = jnp.einsum('...i,...ij,...j->ij', std, obj.cov, std)
+#         self.cache[id(obj)] = value
+#         return value
+
+
+class psi_on_demand():
+    """Descriptor to lazily compute psi."""
+
+    def __init__(self):
+        """Initialize empty cache."""
+        self.cache = {}
+    
+    def __get__(self, obj, objtype=None):
+        value = self.cache.get(id(obj), None)
+        if value is not None:
+            return value
+        value = obj.cov - jnp.outer(obj.theta, obj.theta)
+        self.cache[id(obj)] = value
+        return value
+
+
+class omega_on_demand():
+    """Descriptor to lazily compute omega."""
+
+    def __init__(self):
+        """Initialize empty cache."""
+        self.cache = {}
+    
+    def __get__(self, obj, objtype=None):
+        value = self.cache.get(id(obj), None)
+        if value is not None:
+            return value
+        _fr = jnp.append(obj.cov, jnp.expand_dims(obj.theta, 1), axis=1)
+        _sr = jnp.expand_dims(jnp.append(obj.theta, 1.), 0)
+        value = jnp.append(_fr, _sr, axis=0)
+        self.cache[id(obj)] = value
+        return value
+
+
+class alpha_on_demand():
+    """Descriptor to lazily compute alpha."""
+
+    def __init__(self):
+        """Initialize empty cache."""
+        self.cache = {}
+    
+    def __get__(self, obj, objtype=None):
+        value = self.cache.get(id(obj), None)
+        if value is not None:
+            return value
+        num = jnp.linalg.solve(obj.psi, obj.theta)
+        den = jnp.sqrt(1 + jnp.matmul(obj.theta.T, num))
+        value = num / den
+        self.cache[id(obj)] = value
+        return value
 
 
 class SkewNormal():
@@ -77,26 +101,28 @@ class SkewNormal():
           Compute the log probability distribution
     """
 
-    theta = ComputeOnDemand()
-    Psi = ComputeOnDemand()
-    Omega = ComputeOnDemand()
-    alpha = ComputeOnDemand()
+    # cor = corr_on_demand()
+    theta = theta_on_demand()
+    psi = psi_on_demand()
+    omega = omega_on_demand()
+    alpha = alpha_on_demand()
 
     def __init__(self, mean=jnp.array([0.]),
-                 scale=jnp.array([1.]), cor=jnp.identity(1),
+                 cov=jnp.identity(1),
                  skew=jnp.array([0.])):
         """Check dimensions compatibility and initialize the parameters."""
-        assert mean.shape[-1] == scale.shape[-1]
-        assert cor.shape[-1] == cor.shape[-2]
+        assert mean.shape[-1] == cov.shape[-1]
+        assert cov.shape[-1] == cov.shape[-2]
         assert mean.shape[-1] == skew.shape[-1]
 
         # assert (jnp.linalg.eigvalsh(cor - jnp.outer(skew, skew))).any() > 0
 
         self.mean = mean
-        self.cor = cor
-        self.scale = scale
+        self.cov = cov
         self.skew = skew
         self.k = mean.shape[-1]
+
+        self.scale = jnp.sqrt(jnp.diag(self.cov))
 
     def sample(self, key, shape=None):
         """
@@ -115,11 +141,11 @@ class SkewNormal():
         """
         if shape is None:
             shape = (1,)
-        mu = jnp.append(self.mean, 0.)
+        xi = jnp.append(self.mean, 0.)
         X = random.multivariate_normal(
             key=key, shape=shape,
-            mean=mu,
-            cov=self.Omega,
+            mean=xi,
+            cov=self.omega,
             )
         X0 = jnp.expand_dims(X[:, -1], 1)
         X = X[:, :-1]
@@ -132,24 +158,22 @@ class SkewNormal():
 
     def logpdf(self, z):
         """Compute the logpdf from sample z."""
-        yhat = z - self.mean
         capital_phi = jnp.sum(norm.logcdf(
-            jnp.matmul(self.alpha, yhat.T)))
+            jnp.matmul(self.alpha, (z - self.mean).T)))
         small_phi = jnp.sum(
             mvn.logpdf(
-                yhat,
-                mean=jnp.zeros(shape=(self.k)),
-                cov=self.Psi)
+                z,
+                mean=self.mean,
+                cov=self.psi - jnp.outer(self.alpha, self.alpha))
             )
-        expterm = jnp.sum(0.5 * jnp.matmul(self.alpha, yhat.T))
-        return 2 + small_phi + capital_phi + expterm
+        return 2 + small_phi + capital_phi
 
 
 if __name__ == '__main__':
     from jax.config import config
     config.update('jax_enable_x64', True)
 
-    seed = 42
+    seed = 0
     rng = random.PRNGKey(seed)
 
     import pandas as pd
@@ -158,63 +182,85 @@ if __name__ == '__main__':
     sns.set_style('darkgrid')
 
     p = 2
-    n = 1000
+    n = 100
+    tries = 1000
+    nans = []
+    norms = []
+    
+    for _ in range(tries):
+        rng, *key = random.split(rng, 4)
+        mean = jnp.zeros(shape=(p,))
+        cov = random.uniform(key[1], shape=(p, p)) * 2
+        cov = jnp.matmul(cov, jnp.swapaxes(cov, -2, -1))
+        limskew = min(jnp.linalg.eigvalsh(cov)) / max(jnp.sqrt(jnp.diag(cov)))
+        skew = random.uniform(key[2], shape=(p,), maxval=limskew)
+        
+    
+        # print('Mean: ', mean)
+        # print('Covariance: ', cov)
+        # print('Skewness: ', skew)
+        
+        sn = SkewNormal(mean=mean, cov=cov, skew=skew)
 
-    rng, *key = random.split(rng, 5)
-    mean = jnp.zeros(shape=(p,))
-    random_gen = random.uniform(key[2], shape=(2 * p,)) * 2
-    scale, skew = random_gen[:p], random_gen[p:]
+        # print('Theta:', sn.theta)
+        # print('Psi: ', sn.psi)
+        # print('Eigvals psi: ', jnp.linalg.eigvalsh(sn.psi))
+        # print('alpha:', sn.alpha)
+        # print('Omega:', sn.omega)
+        # print('Eigvals Omega: ', jnp.linalg.eigvalsh(sn.omega))
 
-    fir = jnp.prod(skew)
-    sec = jnp.sqrt(1 + jnp.prod(skew * skew) - jnp.sum(skew*skew))
-    cor = random.uniform(key[1], minval=fir-sec, maxval=fir+sec)
-
-    cor = jnp.array([[1., cor], [cor, 1.]])
-
-    print('Mean: ', mean)
-    print('Scale: ', scale)
-    print('Correlation: ', cor)
-    print('Skewness: ', skew)
-    print('Eigvals corr: ', jnp.linalg.eigvalsh(cor))
-    print('Eigvals skew: ', jnp.linalg.eigvalsh(jnp.outer(skew, skew)))
-
-    sn = SkewNormal(mean=mean, scale=scale, cor=cor, skew=skew)
-
-    print('Theta:', sn.theta)
-    print('Psi: ', sn.Psi)
-    print('Eigvals psi: ', jnp.linalg.eigvalsh(sn.Psi))
-    print('alpha:', sn.alpha)
-    print('Omega:', sn.Omega)
-    print('Eigvals Omega: ', jnp.linalg.eigvalsh(sn.Omega))
-
-    rng, key = random.split(rng)
-    data = sn.sample(key)
-    data = sn.sample(key, shape=(n,))
-    data = pd.DataFrame(
-        data,
-        columns=['x', 'y']
+        eigs = jnp.linalg.eigvalsh(sn.omega)
+        if (eigs <= 0).any():
+            nans.append(jnp.concatenate([jnp.linalg.eigvalsh(cov), skew]))
+            print('nan!\t:(')
+        else:
+            norms.append(jnp.concatenate([jnp.linalg.eigvalsh(cov), skew]))
+            #print('good!\t:)')    
+    
+    print('good percentage: ', 100. * len(norms) / tries)
+    norm = pd.DataFrame(
+        jnp.array(norms),
+        columns=['eig1', 'eig2', 'skew1', 'skew2'],
         )
-    data_normal = pd.DataFrame(
-        random.multivariate_normal(
-            key,
-            mean=mean,
-            cov=jnp.einsum('j,jk,k->jk', scale, cor, scale),
-            shape=(n*10,)
-            ),
-        columns=['x', 'y']
+    nans = pd.DataFrame(
+        jnp.array(nans),
+        columns=['eig1', 'eig2', 'skew1', 'skew2'],
         )
+    norm['result'] = 'good'
+    nans['result'] = 'nan'
+    df = pd.concat([norm, nans], axis=0)
+    #print(df.groupby('result').describe().round(2).T)
+    #sns.scatterplot(data=df, x='eig1', y='eig2', hue='result')
+    #plt.show()
 
-    # sns.scatterplot(data=data, x='x', y='y', alpha=0.4)
-    sns.kdeplot(data=data, x='x', y='y',
-                fill=True,
-                )
-    sns.kdeplot(data=data_normal, x='x', y='y',
-                levels=[0.15, 0.25, 0.5, 0.75, 0.9],
-                alpha=0.4, color='k', linestyles='--', linewidths=0.8
-                )
-    plt.show()
+    # data_skew = pd.DataFrame(
+    #     data,
+    #     columns=['x', 'y']
+    #     )
+    # data_normal = pd.DataFrame(
+    #     random.multivariate_normal(
+    #         key,
+    #         mean=mean,
+    #         cov=cov,
+    #         shape=(n,)
+    #         ),
+    #     columns=['x', 'y']
+    #     )
+    # data_skew['distribution'] = 'skewnormal'
+    # data_normal['distribution'] = 'normal'
+    # dataset = pd.concat([data_skew, data_normal], axis=0)
+    # # sns.scatterplot(data=data, x='x', y='y', alpha=0.4)
+    # sns.kdeplot(data=dataset, x='x', y='y', hue='distribution',
+    #             levels=[0.15, 0.25, 0.5, 0.75, 0.9],
+    #             fill=True, alpha=0.4
+    #             )
+    # # sns.kdeplot(data=data_normal, x='x', y='y',
+    # #             levels=[0.15, 0.25, 0.5, 0.75, 0.9],
+    # #             alpha=0.4, color='k', linestyles='--', linewidths=0.8
+    # #             )
+    # plt.show()
 
-    sns.jointplot(data=data, x='x', y='y')
-    plt.show()
+    # sns.jointplot(data=dataset, x='x', y='y', hue='distribution', alpha=0.5)
+    # plt.show()
 
-    print(sn.logpdf(data.to_numpy()))
+    # print(sn.logpdf(data))
